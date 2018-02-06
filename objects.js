4bb959a96b70c3bc6ac22c196741492a0bad5e3d
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-06 05:39:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,9 @@
 var playlist = new Object();
 
 function updatePlaylist(playlist, name, title) {
-  return 
+  return Object.assign({}, playlist, {[name]: title});
+}
+
+function removeFromPlaylist(playlist, name) {
+  delete playlist.name;
 }
